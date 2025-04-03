d3bb8ecf789bc2c94ec8e8e125e28b1f0969e9b2
NathanMarceus <nathan.marceus.1@ens.etsmtl.ca>
2025-04-03 15:34:46 -0400

    Page de profil avec modification et supression de compte

---
+++ src/assets/utils/auth.js
@@ -0,0 +1,38 @@
+import jwtDecode from "jwt-decode"; 
+
+export function isAuthenticated() {
+  const token = localStorage.getItem("token");
+
+  if (!token) {
+    return false;
+  }
+
+  try {
+    const decodedToken = jwtDecode(token);
+    const currentTime = Math.floor(Date.now() / 1000); // Temps actuel en secondes
+
+    if (decodedToken.exp && decodedToken.exp < currentTime) {
+      localStorage.removeItem("token"); // Supprime le token expiré
+      localStorage.removeItem("userId");
+      localStorage.removeItem("role");
+      return false;
+    }
+
+    return true;
+  } catch (error) {
+    console.error("Erreur lors du décodage du token :", error);
+    localStorage.removeItem("token"); // Supprime un token invalide
+    return false;
+  }
+}
+
+export function getUserRole() {
+  return localStorage.getItem("role") || null;
+}
+
+export function logout() {
+  localStorage.removeItem("token");
+  localStorage.removeItem("userId");
+  localStorage.removeItem("role");
+  window.location.href = "/login"; // Redirige vers la page de connexion
+}
--- src/router.js
+++ src/router.js
@@ -3,12 +3,14 @@ import HomeView from "./pages/homePage.vue";
 import Login from "./pages/login.vue";
 import Search from "./pages/search.vue";
 import Manager from "./pages/manager.vue";
+import Profil from "./pages/profil.vue";
 //import Flight from "./pages/flightDetail.vue";
 
 const routes = [
   { path: "/", component: HomeView }, // Home Page
   { path: "/login", component: Login }, //login
   { path: "/search", component: Search }, //search
+  { path: "/profil", component: Profil }, // page de profil
   { path: "/manager", component: Manager,
     beforeEnter: (to, from, next) => {
       const role = localStorage.getItem("role");
