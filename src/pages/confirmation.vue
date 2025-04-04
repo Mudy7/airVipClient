2fd0b46ab53da9587a1cbf0f02220e8c75386722
mohamedfawaz <mohamad.ay.f@gmial.com>
2025-04-04 17:50:24 -0400

    paying fully functional

---
+++ src/pages/confirmation.vue
@@ -0,0 +1,13 @@
+<template>
+  <main class="p-10 text-center">
+    <h1 class="text-3xl font-bold text-green-600 mb-4">
+      Merci pour votre réservation !
+    </h1>
+    <p class="text-lg">
+      Vous recevrez un courriel avec les détails de votre vol.
+    </p>
+    <router-link to="/" class="text-primary underline mt-6 block"
+      >Retour à l’accueil</router-link
+    >
+  </main>
+</template>
